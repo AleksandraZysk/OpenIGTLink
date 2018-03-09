e599db4926a41c7ddcdc45ba2fd912af0662cf96
Student <alekzys437@student.polsl.pl>
2018-03-09 13:06:16 +0100

    pierwszy nieistotny commit

--- CMake/igtlTestSocklenT.cxx
+++ CMake/igtlTestSocklenT.cxx
@@ -5,10 +5,11 @@
 # include <sys/socket.h>
 #endif
 
+
 int main()
 {
   /* Test whether getsockname takes socklen_t.  */
   if(getsockname(0, 0, (socklen_t*)0)) return 0;
   if(sizeof (socklen_t)) return 0;
-  return 0;
+  return 0; 
 }
